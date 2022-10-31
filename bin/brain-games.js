ca7209fc78b89015906c315c8ac21c7791eaf70e
Andrey Gulyaykin <gulyaykin@gmail.com>
2022-10-31 15:14:20 +0300

    fix eslint errors

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,6 +1,4 @@
 #!/usr/bin/env node
 import userName from '../src/cli.js';
 
-
-console.log('Hello, ' + userName + '!');
-
+console.log(`Hello, ${userName}!`);
--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,6 @@
 import readlineSync from 'readline-sync';
 
-console.log("Welcome to the Brain Games!");
+console.log('Welcome to the Brain Games!');
 
 const userName = readlineSync.question('May I have your name? ');
 
