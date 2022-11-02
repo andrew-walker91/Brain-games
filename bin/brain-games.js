3a83f7a8c8affca0b48e2d96a608a6d6b7329f6e
Andrey Gulyaykin <gulyaykin@gmail.com>
2022-11-02 16:16:49 +0300

    update brain-games.js

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,7 +1,6 @@
 #!/usr/bin/env node
-import userName from '../src/cli.js';
+/* import userName from '../src/cli.js';
 import brainEven from '../src/games/even.js';
-
-console.log(`Hello, ${userName}!`);
-
-brainEven();
+import brainCalc from '../src/games/calc.js';
+import brainGcd from '../src/games/gcd.js';
+*/
