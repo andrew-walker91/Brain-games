589948365f6db61964e3087cfa45a8b5f1af7f18
Andrey Gulyaykin <gulyaykin@gmail.com>
2022-11-04 15:47:45 +0300

    refact greeting

--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,5 @@
-import readlineSync from 'readline-sync';
+const greeting = () => {
+  console.log('Welcome to the Brain Games!');
+};
 
-console.log('Welcome to the Brain Games!');
-
-const userName = readlineSync.question('May I have your name? ');
-
-export default userName;
+export default greeting;
