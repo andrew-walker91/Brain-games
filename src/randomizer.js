c31efa8bcb193fc10b7fc20f5ed9da0a3896c192
Andrey Gulyaykin <gulyaykin@gmail.com>
2022-11-04 15:45:31 +0300

    delete getRandomOperator

--- src/randomizer.js
+++ src/randomizer.js
@@ -1,6 +1,3 @@
-export const getRandomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+const getRandomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
 
-export const getRandomOperator = () => {
-  const operators = ['+', '-', '*'];
-  return operators[getRandomNumber(0, operators.length - 1)];
-};
+export default getRandomNumber;
